ddef4b4612fde42abf89dc68172a8490f6a9bbe9
ali124ali <ali.1377.ad@gmail.com>
2023-07-19 12:19:52 +0330

    create website app and create templates and static directory also moved template files to these directories

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from website.views import *
+
+app_name = 'website'
+
+urlpatterns = [
+    path('', home_view, name = 'home'),
+    path('contact/', contact_view, name = 'contact'),
+    path('about/', about_view, name = 'about'),
+
+    
+]
