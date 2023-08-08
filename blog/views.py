95427cf55469f4fb8fdad986c82d4d9e0337783a
ali124ali <ali.1377.ad@gmail.com>
2023-08-08 13:53:52 +0330

    fixed pagination of blog home with paginator

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment, Category
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
+from datetime import datetime
 
 
 def blog_view(request, **kwargs):
-    posts = Post.objects.filter(status=True).order_by('-published_date')
+    posts = Post.objects.filter(status=True, published_date__lte=datetime.now()).order_by('-published_date')
+
     if kwargs.get('cat_name') != None:
         posts = Post.objects.filter(status = 1, category__name = kwargs['cat_name'])
-    # paginator = Paginator(posts, 3)
-    # page_number = request.GET.get("page")
+
+    # create pagination of blog
+    posts = Paginator(posts, 3)
+
+    try:
+        page_number = request.GET.get("page")
+        posts = posts.get_page(page_number)
+
+    except EmptyPage:
+        posts = posts.get_page(1)
+
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+
+    except InvalidPage:
+        posts = posts.get_page(1)
+
     content = {'posts' : posts}
     return render(request, 'blog/blog.html', content)
 
 def blog_single_view(request, pid):
     post = get_object_or_404(Post, status=1, id=pid)
+    # posts_cat = Post.
     category = Category.objects.all()
     categories = {}
     for cat in category:
@@ -21,7 +39,7 @@ def blog_single_view(request, pid):
             categories[cat.name] = 1
         
         else:
-            categories[cat.name] += 1
+            categories[cat.name] += 2
 
     content = {'post':post, 'categories':categories}
     return render(request, 'blog/single.html', content)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
+from datetime import datetime
 
 def home_view(request):
-    return render(request, 'website/index.html')
+    posts = Post.objects.filter(status=1, published_date__lte=datetime.now()).order_by("-published_date")[:3]
+    content = {'posts':posts}
+    return render(request, 'website/index.html', content)
 
 def contact_view(request):
     return render(request, 'website/contact.html')
