9da2eac4c4971040ff853686af2deec218e77617
ali124ali <ali.1377.ad@gmail.com>
2023-08-20 00:00:34 +0330

    fixed user login function

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = request.POST['username']
+                password = request.POST['password']
 
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+                
+        form = AuthenticationForm()
+        content = {"form" : form}    
+
+        return render(request, 'accounts/login.html', content)
+    
+    else:
+        return redirect('/')
+            
+
+@login_required
 def logout_view(request):
-    pass
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('/')
+    else:
+        return redirect('/')
+
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+
+            if form.is_valid():
+                form.save()
+                # return redirect('accounts/login/')
+
+        form = UserCreationForm()
+        content = {"form" : form}
+        return render(request, 'accounts/signup.html', content)
+    
+    else:
+        return redirect('/')
